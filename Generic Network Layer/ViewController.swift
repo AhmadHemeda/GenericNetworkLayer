b06a28c276c7cf3ce83bad761f7737524112343f
AhmadHemeda <77977659+AhmadHemeda@users.noreply.github.com>
2024-03-19 12:36:32 +0200

    Initial Commit

---
+++ Generic Network Layer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Generic Network Layer
+//
+//  Created by Ahmad Hemeda on 19/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
